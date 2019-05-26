39274f3adf09cc2ab30ed6302f85320f4677cd15
Kolinjo <kozicmarko@yahoo.com>
2019-05-27 00:34:46 +0200

    StringReversal change

--- src/exercises/StringReversal.java
+++ src/exercises/StringReversal.java
@@ -11,8 +11,28 @@ public class StringReversal extends TestCase {
 	 *       QcXgW9w4wQd=v?hctaw/moc.ebutuoy
 	 */
 
-//	public void test() {
-//		assertEquals("cellardoor", reverseMe("roodrallec"));
-//	}
+	public void test() {
+		assertEquals("cellardoor", reverseMe("roodrallec"));
+		System.out.println(reverseMe("QcXgW9w4wQd=v?hctaw/moc.ebutuoy"));
+	}
+
+	private String reverseMe(String word) {
+		
+		String reverseWord = "";
+		char array []  = word.toCharArray();
+		
+		
+		for (int i = array.length -1; i >=0; i--) {
+			reverseWord += array[i];	
+		System.out.println(reverseWord);
+		}
+		
+		return  reverseWord;
+	}
+	
+	
+
+	
+
 
 }
