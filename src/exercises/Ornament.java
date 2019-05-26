cd156906b28b128763e665345d6659ebea4cf2f1
Kolinjo <kozicmarko@yahoo.com>
2019-05-27 00:33:45 +0200

    Ornament change

--- src/exercises/Ornament.java
+++ src/exercises/Ornament.java
@@ -1,15 +1,35 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /* 
  * Create a class called Decorator that contains an ArrayList called boxOfDecorations.
  *   1. In the Decorator class, hang all the ornaments in the box.
- *   2. Add a color to the Ornament class so that your program prints “Hanging a red ornament”, etc.
+ *   2. Add a color to the Ornament class so that your program prints Hanging a red ornament, etc.
  */
 
 public class Ornament {
 	
-	public void hang(){
-		System.out.println("hanging an ornament");
+	String color;
+
+	public Ornament() {
+		ArrayList<String> colors = new ArrayList<String>();
+		colors.add("blue");
+		colors.add("red");
+		colors.add("green");
+		colors.add("grey");
+		colors.add("orange");
+		colors.add("silver");
+		colors.add("yellow");
+		colors.add("white");
+
+		int random = new Random().nextInt(colors.size());
+		this.color = colors.get(random);
+	}
+
+	public void hang() {
+		System.out.println("Hanging a " + this.color + " ornament");
 	}
 
 }
